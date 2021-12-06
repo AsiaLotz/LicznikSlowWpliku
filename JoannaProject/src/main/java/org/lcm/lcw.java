44ef060a40eea2ceefa0a8033cc537710e334d40
AsiaLotz <67192498+AsiaLotz@users.noreply.github.com>
2021-12-06 15:52:50 +0100

    Update lcw.java

--- JoannaProject/src/main/java/org/lcm/lcw.java
+++ JoannaProject/src/main/java/org/lcm/lcw.java
@@ -1,25 +1,31 @@
 package org.lcm;
 
-import java.io.*;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import java.io.FileOutputStream;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
 
 public class lcw {
 
-  private static boolean ASC = true;
-  private static boolean DESC = false;
+  private static final boolean ASC = true;
+  private static final boolean DESC = false;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Scanner odczyt = new Scanner(System.in);
 
     System.out.println("Podaj sciezke pliku:");
@@ -30,6 +36,7 @@ public class lcw {
       fr = new FileReader(plik);
     } catch (FileNotFoundException e) {
       System.out.println("Pliku nie ma w domu");
+      return;
     }
 
     double fileSizeInBytes = plik.length();
@@ -42,7 +49,7 @@ public class lcw {
       System.out.println("Za gruby, nie przejdzie");
     }
 
-    BufferedReader br = new BufferedReader(fr);
+    BufferedReader br = null;
 
     try {
       String line;
@@ -75,87 +82,79 @@ public class lcw {
       Map<String, Integer> sortedMapDesc = sortByValue(iloscSlow, DESC);
       printMap(sortedMapDesc);
 
-    } catch (
-        IOException e) {
-      e.printStackTrace();
+      liczbaSlow(sortedMapAsc);
+
+    } finally {
+      if (br != null) {
+        br.close();
+      }
     }
   }
 
-  private static Map<String, Integer> sortByValue(Map<String, Integer> ilosc,
-      final boolean porzadek) {
+  private static Map<String, Integer> sortByValue(Map<String, Integer> ilosc, final boolean porzadek) {
     List<Entry<String, Integer>> lista = new LinkedList<>(ilosc.entrySet());
 
     // Sortowanie listy słów w oparciu o wartość liczbową
-    lista.sort((slowa, liczba) -> porzadek ? slowa.getValue().compareTo(liczba.getValue()) == 0
-        ? slowa.getKey().compareTo(liczba.getKey())
-        : slowa.getValue().compareTo(liczba.getValue()) : liczba.getValue().compareTo(slowa.getValue()) == 0
-        ? liczba.getKey().compareTo(slowa.getKey())
-        : liczba.getValue().compareTo(slowa.getValue()));
+    lista.sort((slowa, liczba) -> {
+      if (porzadek) {
+        return slowa.getValue().compareTo(liczba.getValue()) == 0
+            ? slowa.getKey().compareTo(liczba.getKey())
+            : slowa.getValue().compareTo(liczba.getValue());
+      }
+      return liczba.getValue().compareTo(slowa.getValue()) == 0
+          ? liczba.getKey().compareTo(slowa.getKey())
+          : liczba.getValue().compareTo(slowa.getValue());
+    });
     return lista.stream().collect(
         Collectors.toMap(Entry::getKey, Entry::getValue, (a, b) -> b, LinkedHashMap::new));
+  }
 
+  private static void liczbaSlow(Map<String, Integer> sortedMapAsc) throws Exception {
+
+    XSSFWorkbook workbook = new XSSFWorkbook();
+
+    XSSFSheet spreadsheet = workbook.createSheet(" Liczba slow ");
+
+    XSSFRow row;
+
+    Map<String, Object[]> liczbaSlow = new TreeMap<>();
+
+    liczbaSlow.put("1", new Object[]{"Slowo", "Ilosc"});
+    int idx = 2;
+    for (Entry<String, Integer> entry : sortedMapAsc.entrySet()) {
+      liczbaSlow.put(idx++ + "", new Object[]{
+          entry.getKey() + "",
+          entry.getValue() + ""});
+    }
+
+    Set<String> keyid = liczbaSlow.keySet();
+
+    int rowid = 0;
+
+    for (String key : keyid) {
+
+      row = spreadsheet.createRow(rowid++);
+      Object[] objectArr = liczbaSlow.get(key);
+      int cellid = 0;
+
+      for (Object obj : objectArr) {
+        Cell cell = row.createCell(cellid++);
+        cell.setCellValue((String) obj);
+      }
+    }
+
+    FileOutputStream out = new FileOutputStream(
+        new File("C:/Wyniki/liczba slow.xlsx"));
+
+    workbook.write(out);
+    out.close();
+    System.out.println("Plik wygenerowany");
   }
 
   private static void printMap(Map<String, Integer> map) {
     map.forEach((key, value) -> System.out.println(key + " " + value));
   }
-
-  private static class WriteDataToExcel {
-
-    // any exceptions need to be caught
-    private static void liczbaSlow(String[] args) throws Exception
-    {
-      // workbook object
-      XSSFWorkbook workbook = new XSSFWorkbook();
-
-      // spreadsheet object
-      XSSFSheet spreadsheet
-              = workbook.createSheet(" Liczba slow ");
-
-      // creating a row object
-      XSSFRow row;
-
-      // This data needs to be written (Object[])
-      Map<String, Object[]> liczbaSlow
-              = new TreeMap<>();
-
-      liczbaSlow.put(
-              "1",
-              new Object[] { "Slowo", "Ilosc" });
-
-
-
-      Set<String> keyid = liczbaSlow.keySet();
-
-      int rowid = 0;
-
-      // writing the data into the sheets...
-
-      for (String key : keyid) {
-
-        row = spreadsheet.createRow(rowid++);
-        Object[] objectArr = liczbaSlow.get(key);
-        int cellid = 0;
-
-        for (Object obj : objectArr) {
-          Cell cell = row.createCell(cellid++);
-          cell.setCellValue((String)obj);
-        }
-      }
-
-      // .xlsx is the format for Excel Sheets...
-      // writing the workbook into the file...
-      FileOutputStream out = new FileOutputStream(
-              new File("C:/Wyniki/liczba slow.xlsx"));
-
-      workbook.write(out);
-      out.close();
-    }
-  }
-
 }
-
-
 
 //https://www.geeksforgeeks.org/how-to-write-data-into-excel-sheet-using-java/
 
